54bd9a896669692ee8051fb01b27952b3b2022e0
tera park <tera63614@gmail.com>
2022-06-04 15:57:15 +0900

    add ui

--- kiwoom_telegram.py
+++ kiwoom_telegram.py
@@ -1,1 +1,49 @@
+﻿
+
+# 64bit veersion
+# ext librarys
+# pip3 install PyQt5
+# Save as UNICODE UTF-8 signed
+
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QPushButton
+from PyQt5.QtCore import QSize
+from PyQt5.QAxContainer import *
+from PyQt5 import uic
+
+form_class = uic.loadUiType("main_window.ui")[0]
+
+class Kiwoom():
+    def __init__(self):
+        super().__init__()
+        if sys.maxsize > 2**32:
+            prog_id = "KHOPENAPI64.KHOpenAPICtrl.1"
+        else:
+            prog_id = "KHOPENAPI.KHOpenAPICtrl.1"
+
+        self.ocx = QAxWidget(prog_id)
+
+    def CommConnect(self):
+        self.ocx.dynamicCall("CommConnect()")
+
+
+class HelloWindow(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.axWindow = Kiwoom()
+        self.btn_login.clicked.connect(self.btnClicked)
+
+
+    def btnClicked(self):
+        self.axWindow.CommConnect()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = HelloWindow()
+    mainWin.show()
+    sys.exit(app.exec_())
 
